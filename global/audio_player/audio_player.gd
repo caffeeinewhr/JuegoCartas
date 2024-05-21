57e2c4b2cd6406a87bb66674ebac6b5a941ec0e3
Marcos! <marcosrevueltasmonreal.social@gmail.com>
2024-05-21 17:24:54 +0200

    fix: sistema de audio

--- global/audio_player/audio_player.gd
+++ global/audio_player/audio_player.gd
@@ -7,12 +7,12 @@ var isVolumeUp: bool = true
 @onready var music: AudioStreamPlayer = $music_player
 
 func change_volume(volume: float):
-	music.volume_db = volume
+	set_volume_db(volume)
 	
 func play_music(userStream: AudioStream, volume = 0.0):	
 	if isMusicEnabled:
-		music.stream = userStream
-		music.volume_db = volume
+		music.set_stream(userStream)
+		music.set_volume_db(volume)
 		music.play()
 	
 func stop_music():
@@ -23,12 +23,11 @@ func _on_music_player_finished():
 
 func change_mute():
 	if isVolumeUp:
-		music.volume_db = -100
+		music.set_volume_db(-80.0)
 	else:
-		music.volume_db = 0.0
-		
+		music.set_volume_db(0.0)
 	isVolumeUp = not isVolumeUp
-		
+
 func change_play():
 	if isMusicEnabled:
 		music.stop()
--- scenes/pause_menu/pause_menu.gd
+++ scenes/pause_menu/pause_menu.gd
@@ -2,62 +2,50 @@ extends Control
 
 var isPaused: bool = false
 
-
 func _input(event):
 	if event.is_action_pressed("pause"):
 		$Audio/ExitClick.play()
 		pauseMenu()
-		
-			
+
 func pauseMenu():
 	isPaused = !isPaused
 	if isPaused:
 		$".".show()
-		AudioPlayer.change_volume(-7.0)
 		get_tree().paused = true
 	else:
 		$".".hide()
-		AudioPlayer.change_volume(0.0)
 		get_tree().paused = false
-
 
 func _on_music_button_pressed():
 	$Audio/NormalClick.play()
-	if AudioPlayer.isMusicEnabled:
+	if AudioPlayer.isVolumeUp:
 		$VBoxContainer/MusicButton.set_text("Music: OFF") 
 	else:
-		$VBoxContainer/MusicButton.set_text("Music: ON") 		
-	AudioPlayer.change_play()
-	
-	
+		$VBoxContainer/MusicButton.set_text("Music: ON") 
+	AudioPlayer.change_mute()
+
 func _on_resume_button_pressed():
 	$Audio/ExitClick.play()
 	pauseMenu()
-
 
 func _on_quit_button_pressed():
 	$Audio/ExitClick.play()
 	await get_tree().create_timer(0.5).timeout
 	get_tree().quit()
-	
-	
+
 func _on_menu_button_pressed():
 	$Audio/ExitClick.play()
 	get_tree().paused = false
 	SceneTransition.load_scene("res://scenes/main_menu/main_menu.tscn")
-	
 
 func _on_music_button_mouse_entered():
 	$Audio/Hover.play()
 
-
 func _on_resume_button_mouse_entered():
 	$Audio/Hover.play()
 
-
 func _on_menu_button_mouse_entered():
 	$Audio/Hover.play()
-
 
 func _on_quit_button_mouse_entered():
 	$Audio/Hover.play()
