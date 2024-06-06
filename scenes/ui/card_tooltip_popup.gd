aa1d6b60f092e362e1f50f65a763de9e1390eca4
PblAbad <pablosanse13@gmail.com>
2024-06-06 17:44:12 +0200

    Commit AntiMierdon

--- scenes/ui/card_tooltip_popup.gd
+++ scenes/ui/card_tooltip_popup.gd
@@ -3,7 +3,6 @@ extends Control
 
 const CARD_MENU_UI_SCENE := preload("res://scenes/ui/card_menu_ui.tscn")
 
-@export var background_color: Color = Color("000000b0")
 
 @onready var background: ColorRect = $Background
 @onready var tooltip_card: CenterContainer = %TooltipCard
@@ -12,11 +11,10 @@ const CARD_MENU_UI_SCENE := preload("res://scenes/ui/card_menu_ui.tscn")
 func _ready() -> void:
 	for card: CardMenuUI in tooltip_card.get_children():
 		card.queue_free()
-	background.color = background_color
 	
 	hide_tooltip()
 	
-func show_tooltip(card: Card)-> void:
+func show_tooltip(card: Card) -> void:
 	var new_card  := CARD_MENU_UI_SCENE.instantiate() as CardMenuUI
 	tooltip_card.add_child(new_card)
 	new_card.card = card
--- scenes/ui/finBatalla.gd
+++ scenes/ui/finBatalla.gd
@@ -1,5 +1,5 @@
 class_name FinBatalla
-extends Panel
+extends Control
 
 enum Type {WIN, LOSE}
 
