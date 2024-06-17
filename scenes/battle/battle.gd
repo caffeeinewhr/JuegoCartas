9ac902e9dd6f3f352d8186c31dbbb9a8377472fb
Marcos! <marcosrevueltasmonreal.social@gmail.com>
2024-06-17 19:17:36 +0200

    Battle arreglado

--- scenes/battle/battle.gd
+++ scenes/battle/battle.gd
@@ -11,13 +11,14 @@ extends Node2D
 @onready var rewards_layer: CanvasLayer = $Rewards
 @onready var battle_rewards: Node = rewards_layer.get_node("BattleReward")
 
-var isTimerStarted: bool = false
+var is_timer_started: bool = false
 
 func _ready() -> void:
 	AudioPlayer.play_music(preload("res://art/music/pelea.wav"), -3.0)
 	await get_tree().create_timer(0.5).timeout
+	timer_real.wait_time = GlobalData.time_left
 	timer_real.start()
-	isTimerStarted = true
+	is_timer_started = true
 	var new_stats: CharacterStats = char_stats.create_instance()
 	battle_ui.char_stats = new_stats
 	player.stats = new_stats
@@ -40,7 +41,7 @@ func _ready() -> void:
 	rewards_layer.hide()
 
 func _process(_delta):
-	if (isTimerStarted):
+	if is_timer_started:
 		GlobalData.set_time_left(timer_real.time_left)
 		timer_ui.update_label(GlobalData.time_left)
 
@@ -51,7 +52,6 @@ func start_battle(stats: CharacterStats) -> void:
 func _on_enemies_child_order_changed() -> void:
 	if enemy_handler.get_child_count() == 0:
 		GlobalData.increase_kills(1)
-		GlobalData.complete_level(GlobalData.current_level)
 		Events.fin_batalla_request.emit("Victoria!!", FinBatalla.Type.WIN)
 
 func _on_enemy_turn_ended() -> void:
@@ -63,13 +63,14 @@ func _on_player_died() -> void:
 	Events.fin_batalla_request.emit("Derrota", FinBatalla.Type.LOSE)
 	
 func _on_timer_timeout():
-	if (isTimerStarted):
+	if is_timer_started:
 		GlobalData.increase_deaths(1)
 		Events.fin_batalla_request.emit("Derrota", FinBatalla.Type.LOSE)
 
 func _on_fin_batalla_request(result: String, type: int):
 	hide_current_scene()
 	if type == FinBatalla.Type.WIN:
+		GlobalData.complete_level()
 		show_battle_reward()
 	else:
 		show_fin_batalla(result)
@@ -79,13 +80,17 @@ func hide_current_scene():
 	self.hide()
 
 func show_battle_reward():
-	rewards_layer.show()
-	battle_rewards.visible = true  # Asegurarse de que el nodo de recompensas esté visible
+	if is_instance_valid(rewards_layer):
+		rewards_layer.show()
+		if is_instance_valid(battle_rewards):
+			battle_rewards.visible = true  # Asegurarse de que el nodo de recompensas esté visible
 
-func show_fin_batalla(result: String):
-	rewards_layer.show()
-	battle_rewards.visible = false  # Asegurarse de que el nodo de recompensas esté oculto si es necesario
-	# Cargar y mostrar la escena de fin de batalla
-	var fin_batalla_scene = preload("res://scenes/battle_reward/battle_reward.tscn").instantiate()
-	rewards_layer.add_child(fin_batalla_scene)
-	fin_batalla_scene.set_result(result)
+func show_fin_batalla(result: String): 
+	if is_instance_valid(rewards_layer):
+		rewards_layer.show()
+		if is_instance_valid(battle_rewards):
+			battle_rewards.visible = false  # Asegurarse de que el nodo de recompensas esté oculto si es necesario
+		# Cargar y mostrar la escena de fin de batalla
+		var fin_batalla_scene = preload("res://scenes/battle_reward/battle_reward.tscn").instantiate()
+		rewards_layer.add_child(fin_batalla_scene)
+		fin_batalla_scene.set_result(result)
